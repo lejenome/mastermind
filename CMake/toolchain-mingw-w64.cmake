68f83d97d58cd1c0b2093bbae4ac2129f7ff4d25
Moez Bouhlel <bmoez.j@gmail.com>
2015-03-23 18:27:26 +0100

    add mingw-w64 toolchain reorganize windows include/lib dirs add FindSDL2.cmake

---
+++ CMake/toolchain-mingw-w64.cmake
@@ -0,0 +1,19 @@
+# based on Archlinux recommondations: https://wiki.archlinux.org/index.php/MinGW_package_guidelines
+SET(CMAKE_SYSTEM_NAME Windows)
+SET(_mingw_w64 "x86_64-w64-mingw32")
+#SET(_mingw_w64 "i686-w64-mingw32")
+#SET(CMAKE_C_FLAGS "-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4")
+
+SET(CMAKE_C_COMPILER ${_mingw_w64}-gcc)
+SET(CMAKE_CXX_COMPILER ${_mingw_w64}-g++)
+SET(CMAKE_RC_COMPILER ${_mingw_w64}-windres)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH "/usr/${_mingw_w64}")
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
