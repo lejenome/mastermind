13c44d439a29f0a262d8d208b174105e396374b1
Moez Bouhlel <bmoez.j@gmail.com>
2015-03-23 01:29:52 +0100

    fix typo

--- src/cli.c
+++ src/cli.c
@@ -60,7 +60,8 @@ void printPanel()
 char *strndup(const char *buf, size_t len)
 {
 	size_t i = strlen(buf);
-	i(i < len) len = i;
+	if (i < len)
+		len = i;
 	char *s = (char *)malloc(sizeof(char) * (len + 1));
 	for (i = 0; i < len; i++)
 		s[i] = buf[i];
