80e46e892c9f898c6611c9e831b78821d5164155
Moez Bouhlel <bmoez.j@gmail.com>
2015-04-03 17:50:39 +0100

    add rainbow_simple & rcg_term & 4bit color schemes

---
+++ colorscheme/rcg_term.h
@@ -0,0 +1,75 @@
+#define fg_text                                                                \
+	{                                                                      \
+		0xCC, 0xCC, 0xCC, 0xFF                                         \
+	}
+#define bg_color                                                               \
+	{                                                                      \
+		0x4B, 0x5D, 0x7E, 0xFF                                         \
+	}
+#define br_color fg_text
+
+#define fg_black                                                               \
+	{                                                                      \
+		0x6c, 0x6c, 0x6c, 0xFF                                         \
+	}
+#define fg_red                                                                 \
+	{                                                                      \
+		0xe9, 0x89, 0x7c, 0xFF                                         \
+	}
+#define fg_green                                                               \
+	{                                                                      \
+		0xb6, 0xe7, 0x7d, 0xFF                                         \
+	}
+#define fg_yellow                                                              \
+	{                                                                      \
+		0xec, 0xeb, 0xbe, 0xFF                                         \
+	}
+#define fg_blue                                                                \
+	{                                                                      \
+		0xa9, 0xcd, 0xeb, 0xFF                                         \
+	}
+#define fg_magenta                                                             \
+	{                                                                      \
+		0xea, 0x96, 0xeb, 0xFF                                         \
+	}
+#define fg_cyan                                                                \
+	{                                                                      \
+		0xc9, 0xca, 0xec, 0xFF                                         \
+	}
+#define fg_white                                                               \
+	{                                                                      \
+		0xCC, 0xCC, 0xCC, 0xFF                                         \
+	}
+
+#define bg_black                                                               \
+	{                                                                      \
+		0x74, 0x74, 0x74, 0xFF                                         \
+	}
+#define bg_red                                                                 \
+	{                                                                      \
+		0xf9, 0x92, 0x86, 0xFF                                         \
+	}
+#define bg_green                                                               \
+	{                                                                      \
+		0xc3, 0xf7, 0x86, 0xFF                                         \
+	}
+#define bg_yellow                                                              \
+	{                                                                      \
+		0xfc, 0xfb, 0xcc, 0xFF                                         \
+	}
+#define bg_blue                                                                \
+	{                                                                      \
+		0xb6, 0xde, 0xfb, 0xFF                                         \
+	}
+#define bg_magenta                                                             \
+	{                                                                      \
+		0xfb, 0xa1, 0xfb, 0xFF                                         \
+	} // purple
+#define bg_cyan                                                                \
+	{                                                                      \
+		0xd7, 0xd9, 0xfc, 0xFF                                         \
+	}
+#define bg_white                                                               \
+	{                                                                      \
+		0xe2, 0xe2, 0xe2, 0xFF                                         \
+	}
