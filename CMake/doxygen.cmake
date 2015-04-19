6e38d5cd5dac8f9b13b678270975ab06f4592424
Moez Bouhlel <bmoez.j@gmail.com>
2015-04-19 09:18:02 +0100

    update doxygen.cmake

--- CMake/doxygen.cmake
+++ CMake/doxygen.cmake
@@ -7,14 +7,6 @@ if(DOXYGEN_FOUND)
 		COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/doc/Doxyfile
      		#DEPENDS  ${headers}
 	)
-      	add_custom_target(doc
-	    	DEPENDS ${CMAKE_BINARY_DIR}/html/index.html
-	    	COMMENT "Building doxygen documentation"
-	)
-      	add_custom_target(html
-	    	DEPENDS ${CMAKE_BINARY_DIR}/html/index.html
-	    	COMMENT "Building doxygen documentation"
-	)
 	add_custom_command(
 		OUTPUT ${CMAKE_BINARY_DIR}/Mastermind.pdf
 		DEPENDS ${CMAKE_BINARY_DIR}/latex/Makefile
@@ -23,11 +15,20 @@ if(DOXYGEN_FOUND)
 		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/latex
      		#DEPENDS  ${headers}
 	)
+      	add_custom_target(doc
+	    	DEPENDS ${CMAKE_BINARY_DIR}/html/index.html
+			${CMAKE_BINARY_DIR}/Mastermind.pdf
+	    	COMMENT "Building doxygen documentation"
+	)
+      	add_custom_target(html
+	    	DEPENDS ${CMAKE_BINARY_DIR}/html/index.html
+	    	COMMENT "Building doxygen documentation"
+	)
       	add_custom_target(pdf
 		DEPENDS ${CMAKE_BINARY_DIR}/Mastermind.pdf
 	    	COMMENT "Building doxygen documentation"
 	)
       	install(DIRECTORY ${CMAKE_BINARY_DIR}/html
-      		DESTINATION share/doc
+      		DESTINATION share/doc/mastermind/
 	)
 endif()
