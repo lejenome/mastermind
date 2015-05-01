e2a31e4ba11c70dd72436e1dfd2d97aae096e0eb
Moez Bouhlel <bmoez.j@gmail.com>
2015-05-01 22:09:46 +0100

    update cmake script

--- CMake/mm_docs.cmake
+++ CMake/mm_docs.cmake
@@ -31,26 +31,26 @@ if(DOXYGEN_FOUND)
       	install(DIRECTORY ${CMAKE_BINARY_DIR}/html
       		DESTINATION share/doc/mastermind/
 	)
-
-      	add_custom_target(manual
-		DEPENDS ${CMAKE_BINARY_DIR}/manual.pdf
-			${CMAKE_BINARY_DIR}/manual/index.html
-			${CMAKE_BINARY_DIR}/manual.txt
-		COMMENT "Building Manual files"
-	)
-	add_custom_command(
-		OUTPUT ${CMAKE_BINARY_DIR}/manual.pdf
-		COMMAND texi2dvi --pdf ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi --output ${CMAKE_BINARY_DIR}/manual.pdf
-		DEPENDS  ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi
-	)
-	add_custom_command(
-		OUTPUT ${CMAKE_BINARY_DIR}/manual.txt
-		COMMAND makeinfo --plaintext ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi --output ${CMAKE_BINARY_DIR}/manual.txt
-		DEPENDS  ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi
-	)
-	add_custom_command(
-		OUTPUT ${CMAKE_BINARY_DIR}/manual/index.html
-		COMMAND makeinfo --html ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi --output ${CMAKE_BINARY_DIR}/manual/
-		DEPENDS  ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi
-	)
 endif()
+
+add_custom_target(manual
+	DEPENDS ${CMAKE_BINARY_DIR}/manual.pdf
+		${CMAKE_BINARY_DIR}/manual/index.html
+		${CMAKE_BINARY_DIR}/manual.txt
+	COMMENT "Building Manual files"
+)
+add_custom_command(
+	OUTPUT ${CMAKE_BINARY_DIR}/manual.pdf
+	COMMAND texi2dvi --pdf ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi --output ${CMAKE_BINARY_DIR}/manual.pdf
+	DEPENDS  ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi
+)
+add_custom_command(
+	OUTPUT ${CMAKE_BINARY_DIR}/manual.txt
+	COMMAND makeinfo --plaintext ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi --output ${CMAKE_BINARY_DIR}/manual.txt
+	DEPENDS  ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi
+)
+add_custom_command(
+	OUTPUT ${CMAKE_BINARY_DIR}/manual/index.html
+	COMMAND makeinfo --html ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi --output ${CMAKE_BINARY_DIR}/manual/
+	DEPENDS  ${CMAKE_CURRENT_SOURCE_DIR}/doc/mastermind.texi
+)
